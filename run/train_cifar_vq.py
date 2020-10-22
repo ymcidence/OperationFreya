b8048b77b68819910547c449eddebfb798d26d08
yumishen <ymcidence@gmail.com>
2020-10-22 11:12:56 +0800

    simagasdasdasd

--- run/train_cifar_vq.py
+++ run/train_cifar_vq.py
@@ -63,10 +63,10 @@ if __name__ == '__main__':
     # noinspection PyUnresolvedReferences
     tf.config.run_functions_eagerly(False)
     set_name = 'cifar10'
-    name = 'vq4000_new'
-    restore_file = os.path.join(ROOT_PATH, 'result', set_name, 'model', 'consistency_05Oct-1318', '_149999-30')
+    name = 'vq4000_cont'
+    restore_file = os.path.join(ROOT_PATH, 'result', set_name, 'model', 'vq4000_new_21Oct-0314', '_499999-100')
     share_encoder = True
     batch_size = [100, 150]
     num_labeled = 4000
     main(name=name, batch_size=batch_size, max_iter=500000, set_name=set_name, num_labeled=num_labeled,
-         share_encoder=share_encoder, restore=None)
+         share_encoder=share_encoder, restore=restore_file)
